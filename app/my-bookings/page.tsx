4bf4f0bcb4749f163f07610531fc78a74a56364c
jannick <jannickpedersen@live.dk>
2024-02-01 12:58:26 +0100

    fixed some components name changes pt6

--- app/my-bookings/page.tsx
+++ app/my-bookings/page.tsx
@@ -3,6 +3,7 @@ import { getBookingsByHotelOwnerId } from "@/actions/getBookingsByHotelOwnerId";
 import { getBookingsByUserId } from "@/actions/getBookingsByUserId";
 import MyBookingClient from "../../components/booking/MyBookingsClient";
 
+
 const MyBookings = async () => {
 
     const bookingsFromVisitors = await getBookingsByHotelOwnerId();
